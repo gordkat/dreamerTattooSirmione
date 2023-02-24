0eb4d3aff92da72b472e0cda05e8508c2e7945a2
gordkat <gordkat@gmail.com>
2023-02-24 11:55:55 +0100

    four

--- src/views/Gallery.jsx
+++ src/views/Gallery.jsx
@@ -4,7 +4,7 @@ import GalleryList from "../components/GalleryList";
 const Gallery = () => {
   return (
     <main className={styles.containerGallery}>
-      <h1 className={styles.titleGallery}>gallery</h1>
+      <h1 className={styles.titleGallery}>gallery!!!</h1>
       <GalleryList />
     </main>
   );
